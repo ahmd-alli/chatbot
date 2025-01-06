d0bd425fda732ec2505f0156a299bbe8b38b4a06
ahmad <ahmd.ali.sabir@gmail.com>
2025-01-06 14:36:34 +0500

    using LCEL

--- backend/interactors/chat.py
+++ backend/interactors/chat.py
@@ -3,27 +3,38 @@ from backend.utils.str import String
 from backend.service.llm import LLM
 from backend.service.pinecone import PineconeDB
 from backend.utils.openai import Embedder as Openai_Embedder
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
 
-def call(question :str):
+def call(question: str) -> str:
     # Get embedder.
     embedder = Openai_Embedder.get()
-    
+
     filename = os.getenv("KNOWLEDGE_FILE_NAME")
-    
+
     # Get matching chunks from db.
     matching_chunks = PineconeDB.retrieve(question, embedder, filename.split('.')[0])
-    
+
     # Convert documents list to text.
     txt = String.document_to_str(matching_chunks)
-    
+
     # Prepare prompt.
-    prompt = """The document text below is taken from a person's resume. Please read it carefully and answer the below question. Please don't give an answer outside of this document text. also, format the answer if required.
+    template = """The document text below is taken from a person's resume. Please read it carefully and answer the below question. Please don't give an answer outside of this document text. Also, format the answer if required.
 
         Document Text: {txt}
         Question: {question}
-    """.format(txt=txt, question=question)
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+
+    # Get model.
+    model = LLM.get()
+
+    # Instantiate the output parser.
+    output_parser = StrOutputParser()
+
+    # Build the chain properly with the components in sequence.
+    chain =  prompt | model | output_parser
     
-    # Ask LLM.
-    llm = LLM.get()
-    return llm.invoke(prompt).content 
-    
+    # Prepare the input for the chain and invoke it.
+    return chain.invoke({"txt": txt, "question": question})
