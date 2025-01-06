98969b4bd0c76419003691bac122de62c9cba80c
ahmad <ahmd.ali.sabir@gmail.com>
2025-01-06 15:22:24 +0500

    using genai model

--- backend/interactors/chat.py
+++ backend/interactors/chat.py
@@ -2,7 +2,7 @@ import os
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from backend.utils.str import String
-from backend.service.llm import LLM
+from backend.service.google import LLM
 from backend.service.pinecone import PineconeDB
 from backend.utils.openai import Embedder as Openai_Embedder
 
@@ -19,21 +19,15 @@ def call(question: str) -> str:
     txt = String.document_to_str(matching_chunks)
 
     # Prepare prompt.
-    template = """The document text below is taken from a person's resume. Please read it carefully and answer the below question. Please don't give an answer outside of this document text. Also, format the answer if required.
+    prompt = f"""The document text below is taken from a person's resume. Please read it carefully and answer the below question. Please don't give an answer outside of this document text. Also, format the answer if required.
 
         Document Text: {txt}
         Question: {question}
     """
-    prompt = ChatPromptTemplate.from_template(template)
 
     # Get model.
     model = LLM.get()
 
-    # Instantiate the output parser.
-    output_parser = StrOutputParser()
-
-    # Build the chain properly with the components in sequence.
-    chain =  prompt | model | output_parser
-    
-    # Prepare the input for the chain and invoke it.
-    return chain.invoke({"txt": txt, "question": question})
+    # Ask.
+    ans = model.generate_content(prompt)
+    return  ans.text
---
+++ backend/service/google.py
@@ -0,0 +1,7 @@
+import os
+import google.generativeai as genai
+
+class LLM:
+  def get(model_name = "gemini-1.5-flash"):
+      genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+      return genai.GenerativeModel(model_name)
